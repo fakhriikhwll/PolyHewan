1258b16fd26a4f8f5e861ba74e62710fae59529e
fakhriikhwll <48468766+fakhriikhwll@users.noreply.github.com>
2019-05-14 14:46:34 +0700

    Add files via upload

---
+++ Chicken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    class Chiken : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("Chiken says\t: Kukuruyuk");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Speak();
+
+            Cat cat = new Cat();
+            cat.Speak();
+            //animal.Speak();
+
+            Chiken chiken = new Chiken();
+            chiken.Speak();
+            //animal.Speak();
+
+            Dog dog = new Dog();
+            dog.Speak();
+            //animal.Speak();
+
+            Console.ReadKey();
+        }
+    }
+}
